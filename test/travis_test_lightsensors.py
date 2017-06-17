1c61ec9a35dec8f42c101bd43e3b088ccf0372f7
Ubuntu <keisuke.031107@gmail.com>
2017-06-17 14:43:19 +0000

    modefied travis_test_lightsensors.py

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -50,6 +50,6 @@ class LightsensorTest(unittest.TestCase):
 
 if __name__ == '__main__':
     time.sleep(3)
-    rospy.init_node('travias_test_lightsensors')
+    rospy.init_node('travis_test_lightsensors')
     rostest.rosrun('pimouse_ros','travis_test_lightsensors',LightsensorTest)
    
