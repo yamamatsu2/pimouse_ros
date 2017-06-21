9e1d1a90f5d5dbe0ce486d644fb30d27b1ab01ab
Ubuntu <keisuke.031107@gmail.com>
2017-06-21 10:19:47 +0000

    seto daiya

--- test/travis_test_motors2.py
+++ test/travis_test_motors2.py
@@ -5,6 +5,7 @@ import rosnode, rospy
 import time
 from pimouse_ros.msg import MotorFreqs
 from geometry_msgs.msg import Twist
+from std_srvs.srv import Trigger, TriggerResponse
 
 class MotorTest(unittest.TestCase):
     def file_check(self,dev,value,message):
@@ -42,6 +43,30 @@ class MotorTest(unittest.TestCase):
         time.sleep(1.1)
         self.file_check("rtmotor_raw_r0",0,"don't stop after 1[s]")
         self.file_check("rtmotor_raw_l0",0,"don't stop after 1[s]")
+
+    def setUp(self):
+        rospy.wait_for_service('/motor_on')
+        rospy.wait_for_service('/motor_off')
+        on = rospy.ServiceProxy('/motor_on', Trigger)
+        ret = on()
+
+    def test_on_off(self):
+        off = rospy.ServiceProxy('/motor_off', Trigger)
+        ret = off()
+        self.assertEqual(ret.success, True, "motor off does not succeeded")
+        self.assertEqual(ret.message, "OFF", "motor off wrong message")
+        with open("/dev/rtmotoren0","r") as f:
+            data = f.readline()
+            self.assertEqual(data,"0\n","wrong value in rtmotor0 at motor off")
+
+        on = rospy.ServiceProxy('/motor_on', Trigger)
+        ret = on()
+        self.assertEqual(ret.success, True, "motor on does not succeeded")
+        self.assertEqual(ret.message, "ON", "motor on wrong message")
+        with open("/dev/rtmotoren0","r") as f:
+            data = f.readline()
+            self.assertEqual(data,"1\n","wrong value in rtmotor0 at motor on")
+
 
 if __name__ == '__main__':
     time.sleep(3)
