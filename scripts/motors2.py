114c95e04f30a7c23d844c63dc08b09b65758d96
Ubuntu <keisuke.031107@gmail.com>
2017-06-21 13:36:57 +0000

    8.2.2-2

--- scripts/motors2.py
+++ scripts/motors2.py
@@ -4,6 +4,7 @@ import sys, rospy, math
 from pimouse_ros.msg import MotorFreqs
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
+from pimouse_ros.srv import TimedMotion
 
 class Motor():
     def __init__(self):
@@ -14,6 +15,7 @@ class Motor():
         self.sub_cmd_vel = rospy.Subscriber('cmd_vel', Twist, self.callback_cmd_vel)
         self.srv_on = rospy.Service('motor_on', Trigger, self.callback_on)
         self.srv_off = rospy.Service('motor_off', Trigger, self.callback_off)
+        self.srv_tm = rospy.Service('timed_motion', TimedMotion, self.callback_tm)
         self.last_time = rospy.Time.now()
         self.using_cmd_vel = False
 
@@ -60,6 +62,22 @@ class Motor():
 
     def callback_on(self,message): return self.onoff_response(True)
     def callback_off(self,message): return self.onoff_response(False)    
+
+    def callback_tm(self,message):
+	if not self.is_on:
+	    rospy.logerr("not enpowered")
+            return False
+
+        dev = "/dev/rtmotor0"
+        try:
+            with open(dev,'w') as f:
+	        f.write("%d %d %d\n" %
+                    (message.left_hz,message.right_hz,message.duration_ms))
+        except:
+            rospy.logerr("cannot write to " + dev)
+            return False
+
+        return True
 
 if __name__ == '__main__':
     rospy.init_node('motors')
