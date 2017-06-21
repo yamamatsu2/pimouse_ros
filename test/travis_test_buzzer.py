251f98d48b5496e4c7c48fd44ee6726e8029d1d8
Ubuntu <keisuke.031107@gmail.com>
2017-06-21 15:24:58 +0000

    8.3.4

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -35,8 +35,7 @@ class BuzzerTest(unittest.TestCase):
         self.client.wait_for_result()
 
         self.assertTrue(self.client.get_result(),"invalid result")
-        self.assertEqual(goal.freqs,self.device_values,"invalid feedback:"
-                + ",".join([str(e) for e in self.device_values]))
+        self.assertEqual(goal.freqs,self.device_values,"invalid feedback:" + ",".join([str(e) for e in self.device_values]))
 
         self.device_values = []
         self.client.send_goal(goal,feedback_cb=self.feedback_cb)
